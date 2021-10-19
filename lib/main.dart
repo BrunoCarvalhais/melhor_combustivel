44f75bb0dc800c20fb464a124d9b6ce7a789478c
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-10-19 15:09:23 -0300

    commitproject2

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:melhor_combustivel/home.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
